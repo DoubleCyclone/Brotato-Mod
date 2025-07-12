6652e0d6efd90b83973f30d57a530d8ab0fe8f71
Umut Bekdemir <umut.bekdemir29@gmail.com>
2025-07-13 01:28:14 +0300

    added a new explosion type which adds effects from itself to the hitbox

---
+++ projectiles/radial_player_explosion.gd
@@ -0,0 +1,13 @@
+class_name RadialPlayerExplosion
+extends PlayerExplosion
+
+export (Array, Resource) var explosion_effects
+
+func start_explosion() -> void :
+	show()
+	_hitbox.effects = explosion_effects.duplicate()
+	_hitbox.enable()
+	_hitbox.from = self
+	_sprite.modulate.a = ProgressData.settings.explosion_opacity
+	_animation_player.play("explode")
+	set_physics_process(true)
