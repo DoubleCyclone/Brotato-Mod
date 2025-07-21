77ce2b12448faa2751c47fa3542cbf76b807022f
Umut Bekdemir <umut.bekdemir29@gmail.com>
2025-07-21 14:15:07 +0300

    added translation

--- effects/weapons/thrown_bomb_effect.gd
+++ effects/weapons/thrown_bomb_effect.gd
@@ -8,3 +8,13 @@ export (float) var timer_cooldown
 static func get_id() -> String:
 	return "effect_hyper_bomb_spawn"
 	
+func get_args(_player_index: int) -> Array:
+	var scale
+	for effect in effects:
+		if effect.key == "effect_explode":
+			if RunData.players_data[_player_index].effects["explosion_size"] != 0:				
+				scale = effect.scale * (RunData.players_data[_player_index].effects["explosion_size"] / 100)
+			else:
+				scale = effect.scale
+	return [str(timer_cooldown), str(scale)]
+	
