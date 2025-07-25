caea47ef6bc3a61d90ba1c82c4a614875ec69fa9
Umut Bekdemir <umut.bekdemir29@gmail.com>
2025-07-25 15:10:44 +0300

    trajectory coded, effect and translation added, make higher tiers

---
+++ effects/weapons/rolling_cutter_boomerang_effect.gd
@@ -0,0 +1,11 @@
+class_name RollingCutterBoomerangEffect
+extends NullEffect
+
+export (Resource) var weapon_stats
+
+static func get_id() -> String:
+	return "weapon_boomerang"
+
+func get_args(_player_index: int) -> Array:
+	var current_stats = WeaponService.init_ranged_stats(weapon_stats, _player_index, true)
+	return [str(round(current_stats.projectile_speed)), str(round(current_stats.max_range / 3)), str(round((current_stats.max_range / 3) * 0.75))]
