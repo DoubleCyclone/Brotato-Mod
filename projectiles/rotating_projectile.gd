e2293e71b7cc5fd2aa157a91e05569ea0249fdde
Umut Bekdemir <umut.bekdemir29@gmail.com>
2025-09-19 19:55:36 +0300

    rotating projectiles now have a random offset starting point for better visuals

--- projectiles/rotating_projectile.gd
+++ projectiles/rotating_projectile.gd
@@ -4,20 +4,19 @@ extends PlayerProjectile
 export(float) var radius
 export(int) var max_rotation
 var d = 0
-var distance_taken = 0 #should be a better way with this one
-#var rotating = true
+var distance_taken = 0
 var origin_object
 var rotating_speed = 0
+var offset = randf() * TAU
 
 
 func _physics_process(delta: float) -> void :
-#	if rotating and rotating_speed != 0:
 	if origin_object == null:
 		origin_object = _hitbox.from.get_parent().get_parent()
 	var max_range = PI * 2 * radius * max_rotation
 	var previous_position = global_position
 	d += delta
-	position = origin_object.position + Vector2(sin(d * rotating_speed) * radius, cos(d * rotating_speed) * radius)
+	position = origin_object.position + Vector2(cos(d * rotating_speed + offset) * radius, sin(d * rotating_speed + offset) * radius)
 	var distance_this_frame = global_position.distance_to(previous_position)
 	# calculate distance taken every frame so that we can delete it when it exceeds max range (not the max range stat of the projectile)
 	distance_taken += distance_this_frame
