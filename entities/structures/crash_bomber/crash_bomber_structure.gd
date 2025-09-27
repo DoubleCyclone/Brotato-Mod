8cd20379d8ab7c640949651dbbf12023a78b5b05
Umut Bekdemir <umut.bekdemir29@gmail.com>
2025-09-27 11:24:33 +0300

    structure initialized, sprites drawn

---
+++ entities/structures/crash_bomber/crash_bomber_structure.gd
@@ -0,0 +1,47 @@
+class_name CrashBomberStructure
+extends Structure
+
+var cooldown = 0.25
+var from_weapon
+
+
+func _ready() -> void :
+	._ready()
+	get_tree().create_timer(cooldown,false).connect("timeout",self,"on_explosion_timer_run_out")
+	
+	
+func respawn() -> void :
+	.respawn()
+	get_tree().create_timer(cooldown,false).connect("timeout",self,"on_explosion_timer_run_out")
+
+
+func on_explosion_timer_run_out() -> void:
+	if dead or effects.size() <= 0: return
+
+	var explosion_effect = effects[0]
+	var args: = WeaponServiceExplodeArgs.new()
+	args.pos = global_position
+	args.damage = stats.damage
+	args.accuracy = stats.accuracy
+	args.crit_chance = stats.crit_chance
+	args.crit_damage = stats.crit_damage
+	args.burning_data = stats.burning_data
+	args.scaling_stats = stats.scaling_stats
+	args.from_player_index = player_index
+	args.from = from_weapon
+#	args.damage_tracking_key = explosion_effect.tracking_key
+	var _inst = WeaponService.explode(explosion_effect, args)
+	_inst.connect("hit_something",self,"_on_explosion_hit_something")
+	die()
+
+
+func _on_explosion_hit_something(thing_hit, damage_dealt) -> void :
+	RunData.manage_life_steal(from_weapon.current_stats, player_index)
+
+
+func _on_Area2D_body_entered(_body: Node) -> void :
+	pass
+
+
+func _on_Area2D_body_exited(_body: Node) -> void :
+	pass
