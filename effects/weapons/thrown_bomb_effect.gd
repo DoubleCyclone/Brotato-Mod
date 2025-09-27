e1e7f890cf9951c0c3acd3b39086c56f30aab57a
Umut Bekdemir <umut.bekdemir29@gmail.com>
2025-09-27 17:22:26 +0300

    crash bomber structure animation changes do not save for whatever reason

--- effects/weapons/thrown_bomb_effect.gd
+++ effects/weapons/thrown_bomb_effect.gd
@@ -6,7 +6,7 @@ export (PackedScene) var structure_scene
 export (float) var timer_cooldown
 
 static func get_id() -> String:
-	return "effect_hyper_bomb_spawn"
+	return "effect_bomb_spawn"
 	
 func get_args(_player_index: int) -> Array:
 #	var scale
--- weapons/shooting_behaviors/attach_structure_shooting_behavior.gd
+++ weapons/shooting_behaviors/attach_structure_shooting_behavior.gd
@@ -1,13 +1,10 @@
-class_name ThrowStructureShootingBehavior
+class_name AttachStructureShootingBehavior
 extends WeaponShootingBehavior
 
 signal projectile_shot(projectile)
 
-export (Texture) var throw_sprite
-export (Texture) var cooldown_sprite
 var structure_scene
 var structure_spawn_effect
-var original_lifesteal
 
 func shoot(_distance: float) -> void :
 	for effect in _parent.effects:
@@ -16,7 +13,6 @@ func shoot(_distance: float) -> void :
 			structure_scene = effect.structure_scene
 			
 	SoundManager.play(Utils.get_rand_element(_parent.current_stats.shooting_sounds), _parent.current_stats.sound_db_mod, 0.2)
-	_parent.get_node("Sprite").texture = throw_sprite
 
 	var initial_position: Vector2 = _parent.sprite.position
 
@@ -26,11 +22,9 @@ func shoot(_distance: float) -> void :
 	for i in _parent.current_stats.nb_projectiles:
 		var proj_rotation = rand_range(_parent.rotation - _parent.current_stats.projectile_spread, _parent.rotation + _parent.current_stats.projectile_spread)
 		var knockback_direction: = Vector2(cos(proj_rotation), sin(proj_rotation))
-		original_lifesteal = _parent.current_stats.lifesteal
-		_parent.current_stats.lifesteal = 0
 		var projectile = shoot_projectile(proj_rotation, knockback_direction)
 		projectile._hitbox.player_attack_id = attack_id
-		projectile.connect("projectile_stopped",self,"on_projectile_stopped")
+		projectile.connect("hit_something",self,"on_projectile_hit_something", [projectile])
 
 	_parent.tween.interpolate_property(
 		_parent.sprite, 
@@ -58,11 +52,11 @@ func shoot(_distance: float) -> void :
 	_parent.tween.start()
 	yield(_parent.tween, "tween_all_completed")
 
-	_parent.get_node("Sprite").texture = cooldown_sprite
 	_parent.set_shooting(false)
 	
-func on_projectile_stopped(projectile):
-	_parent.current_stats.lifesteal = original_lifesteal
+func on_projectile_hit_something(thing_hit, damage_dealt, projectile):
+	if projectile._piercing != 0:
+		return
 	var instance = structure_scene.instance()
 	instance.from_weapon = _parent
 	instance.player_index = _parent.player_index
@@ -73,7 +67,7 @@ func on_projectile_stopped(projectile):
 		if effect.key == "effect_bomb_spawn":
 			instance.effects = effect.effects #TODO : maybe append
 	instance.cooldown = structure_spawn_effect.timer_cooldown
-	Utils.get_scene_node().get_node("Entities").add_child(instance)
+	Utils.get_scene_node().get_node("Entities").call_deferred("add_child",instance)
 	
 
 func shoot_projectile(rotation: float = _parent.rotation, knockback: Vector2 = Vector2.ZERO) -> Node:
@@ -89,7 +83,6 @@ func shoot_projectile(rotation: float = _parent.rotation, knockback: Vector2 = V
 		_parent, 
 		args
 	)
-	projectile.rotation = 0
 
 	emit_signal("projectile_shot", projectile)
 	return projectile
