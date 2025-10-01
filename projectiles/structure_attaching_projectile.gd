1118ef0b1eae6118223355a530b203fe3ef5dd3b
Umut Bekdemir <umut.bekdemir29@gmail.com>
2025-10-01 20:40:34 +0300

    fixed crash bomber behavior after changes to _time_until_max_range

--- projectiles/structure_attaching_projectile.gd
+++ projectiles/structure_attaching_projectile.gd
@@ -7,10 +7,11 @@ signal projectile_stopped(projectile)
 
 func _physics_process(_delta: float) -> void :
 	# Prevent the projectile from leaving the screen
+	print(_time_until_max_range)
 	if position.x < ZoneService.current_zone_min_position.x or position.x > ZoneService.current_zone_max_position.x:
-		_time_until_max_range = 0
+		_time_until_max_range = -1
 	if position.y < ZoneService.current_zone_min_position.y or position.y > ZoneService.current_zone_max_position.y:
-		_time_until_max_range = 0
+		_time_until_max_range = -1
 
 
 func stop() -> void :
